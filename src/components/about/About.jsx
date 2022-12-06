e85818ac8daa8d182461ede5d3c477ff8e239fab
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-12-06 13:29:31 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css"
 import AboutImg from "../../assets/Sai.jpg";
-import CV from "../../assets/Mohan-Cv.pdf"
+import CV from "../../assets/ANKE-SaiPrasad.pdf"
 import Info from './Info';
 
 const About = () => {
